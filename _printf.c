1c46e535e9adbeadd54cfe3cb3366cf6e748c34a
Aishah <maishah2540@gmail.com>
2023-05-28 09:24:54 -0700

    Edited the _printf.c file to cater for other possible cases of the _printf function

--- _printf.c
+++ _printf.c
@@ -1,15 +1,30 @@
 #include "main.h"
 
 /**
- * _printf - basic printf
+ * _printf - A function that works similar to  printf (in stdio.h library)
  * @format: character string containing directives on how to print
- * Return: 0 on success
+ * Return: the number of characters printed
+ *		(excluding the null byte used to end output to strings
  */
 int _printf(const char *format, ...)
 {
 	int i;
+	va_list args;
+
+	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
-		write(1, format + i, 1);
-	return (0);
+	{
+		if (format[i] == '%s)
+		{
+			i++;
+			convert(format + i)(args);
+		}
+		else
+		{
+			write(1, format + i, 1);
+		}
+	}
+
+	return (i);
 }
