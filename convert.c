a5c5e3b0e7ce7978bd010d0d21bff5cfb9cd14c9
Aishah <maishah2540@gmail.com>
2023-05-28 09:32:52 -0700

    Created the convert.c file

---
+++ convert.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * convert - Function pointer that matches conversion specifier,
+ *		to its correspnding function
+ * @symbol: conversion specifier symbol to be matched
+ *
+ * Return: void
+ */
+
+void (*convert(const char *symbol))(va_list list)
+{
+	formatter1 func[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"i", print_i}
+	};
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (*symbol == *(func[i].letter))
+			return (func[i].f);
+	}
+
+	write(2, "Symbol not found", 17);
+	exit(1);
+}
