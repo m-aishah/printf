496fa82f2aa2c477f9b31e3255942072e1f678d8
Aishah <maishah2540@gmail.com>
2023-05-28 09:19:11 -0700

    Edited header file to cater for other possible cases of _printf function

--- main.h
+++ main.h
@@ -1,21 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
+/**
+ * struct formatter - A structure that called formatter
+ * @letter: conversion specifier (c, s, d, or i)
+ * @f: A function pointer to print functions
+ */
 
 typedef struct formatter
 {
-	char *symbol;
-	void (*f)(char *);
+	char *letter;
+	void (*f)(va_list list);
 } formatter1;
 
-void (*converter(char *))(va_list list);
+int _printf(const char *format, ...);
+void (*convert(char *))(va_list list);
 void print_c(va_list list);
 void print_s(va_list list);
 void print_d(va_list list);
 void print_i(va_list list);
+void print_number(int);
 #endif
