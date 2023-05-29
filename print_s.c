cbc5f08ac974352a7f3edc90ae2ed1adda821887
Deephiliz <philipsdebs@gmail.com>
2023-05-29 03:49:17 -0700

    Wrote function definition for print_s

---
+++ print_s.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_s - Function that prints a string.
+ * @list: A va_list pointing to the string to be printed.
+ */
+
+void print_s(va_list list)
+{
+	char *str;
+	int i;
+
+	str = va_arg(list, char *);
+	if (!str)
+		exit(1);
+	for (i = 0; *(str + i); i++)
+		write(1, (str + i), 1);
+}
